2c7a6d8a5f209f0e1ce0c09026c48a9d6f9135cc
Abady <gamersspeaks@gmail.com>
2019-03-17 18:21:28 +0300

    chore: better deployment

--- deploy/pull.sh
+++ deploy/pull.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-if [ "$NODE_ENV" = "production" ] 
-then
-   git pull origin master
-   echo "[Abayro]: I've deployed the latest build from master(Stable Build)"
-else
-   git pull origin release/v4
-   echo "[Abayro]: I've deployed the latest build from V4 release."
-fi
+git pull origin master
+echo "[Abayro]: I've deployed the latest build from master"
