2acaccd187754740f98f92a5ae5cb4ba748c6e3f
Abady <gamersspeaks@gmail.com>
2019-03-17 18:39:17 +0300

    test: testing deploys

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
-# TODO: Make circleci with scripts to make this work, also improve this V
+# TODO: Make circleci with scripts to make this work, also improve this V, testing
 # echo "[Abayro]: Deploy started"
 # cd abayro
 # sh -ac '. ./.env; ./deploy/pull.sh'
