da7334be91dba8602d73d39e735a7cd8ff0ec1bd
Abady <gamersspeaks@gmail.com>
2019-04-19 07:31:40 +0300

    fix: remove duplicated funcs

--- bot/util/Util.js
+++ bot/util/Util.js
@@ -61,22 +61,6 @@ module.exports.splitMessage = (text, { maxLength = 2000, char = '\n', prepend = 
 	return messages.concat(msg).filter(m => m);
 };
 
-module.exports.splitMessage = (text, { maxLength = 2000, char = '\n', prepend = '', append = '' } = {}) => {
-	if (text.length <= maxLength) return text;
-	const splitText = text.split(char);
-	if (splitText.some(chunk => chunk.length > maxLength)) throw new RangeError('SPLIT_MAX_LEN');
-	const messages = [];
-	let msg = '';
-	for (const chunk of splitText) {
-		if (msg && (msg + char + chunk + append).length > maxLength) {
-			messages.push(msg + append);
-			msg = prepend;
-		}
-		msg += (msg && msg !== prepend ? char : '') + chunk;
-	}
-	return messages.concat(msg).filter(m => m);
-};
-
 /**
  * @param max the max amount
  * @param min the min amount
