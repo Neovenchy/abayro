dcb3eea41b042c93042d9a78a135ba8682cb018d
Abady <gamersspeaks@gmail.com>
2019-04-29 14:46:03 +0300

    chore: improvents to blacklist.

--- bot/inhibitors/blacklist.js
+++ bot/inhibitors/blacklist.js
@@ -1,17 +1,17 @@
 const { Inhibitor } = require('discord-akairo');
 
- class BlacklistInhibitor extends Inhibitor {
- 	constructor() {
- 		super('blacklist', {
- 			type: 'pre',
- 			reason: 'blacklist'
- 		});
- 	}
+class BlacklistInhibitor extends Inhibitor {
+	constructor() {
+		super('blacklist', {
+			type: 'pre',
+			reason: 'blacklist'
+		});
+	}
 
- 	exec(message) {
- 		const blacklisted = this.client.settings.get('global', 'blacklist', []);
- 		return blacklisted.includes(message.author.id);
- 	}
- }
+	exec(message) {
+		const blacklisted = this.client.settings.get('global', 'blacklist', []);
+		return blacklisted.includes(message.author.id);
+	}
+}
 
 module.exports = BlacklistInhibitor;
