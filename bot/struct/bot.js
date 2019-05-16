d8a80f3a2c653e70cb3b5d17d66861907e9375a5
N <38608933+NourEldienHE@users.noreply.github.com>
2019-05-16 23:30:00 +0200

    add new dev

--- bot/struct/bot.js
+++ bot/struct/bot.js
@@ -9,7 +9,7 @@ exports.library = {
 	dj: djsVersion
 };
 
-exports.staff = ['171259176029257728', '556036680352792596'];
+exports.staff = ['171259176029257728', '556036680352792596', '231956829159161856'];
 
 exports.emojis = {
 	yes: ':white_check_mark:',
