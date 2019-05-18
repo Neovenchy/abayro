adf4d4b1dc81c6535e28f029dafe2c47309df5c8
Abady <gamersspeaks@gmail.com>
2019-05-19 01:26:49 +0300

    fix: reset to avoid conflicts

--- deploy/pull.sh
+++ deploy/pull.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+git reset --hard
+echo "[Abayro]: I've rest to the last commit."
 git pull origin master
-echo "[Abayro]: I've deployed the latest build from master"
+echo "[Abayro]: I've deployed the latest build from master."
