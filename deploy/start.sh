8f68862b98cdc6422430e92357eac235e900a0c9
Abady <gamersspeaks@gmail.com>
2019-05-18 21:04:54 +0300

    fix: correct path

--- deploy/start.sh
+++ deploy/start.sh
@@ -2,7 +2,7 @@
 echo "[Abayro]: I'am killing the current build"
 pm2 kill || killall node
 echo "[Abayro]: Install all required deps"
-npm i --production
+yarn Install
 echo "[Abayro]: Prune packages I don't need!"
 npm prune --production
 echo "[Abayro]: Starting...."
