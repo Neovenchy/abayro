c7daadb30328829642bd10e9b9051224084e2f8c
Abady <gamersspeaks@gmail.com>
2019-05-20 02:10:30 +0300

    fix: dashboard command (#42)

--- src/bot/commands/general/dashboard.js
+++ src/bot/commands/general/dashboard.js
@@ -13,29 +13,15 @@ class DashboardCommand extends Command {
 				content: 'To see your server control-panel'
 
 			},
-			args: [{
-				'id': 'guild',
-				'type': 'guild',
-				'default': message => message.guild
-			}]
+			userPermissions: ['MANAGE_GUILD']
 		});
 	}
 
 	/**
  * @param {import('discord.js').Message} message
  */
-	exec(message, { guild }) {
-		if (!guild) {
-			if (!message.guild.member(message.author).hasPermission('MANAGE_GUILD') || message.guild.member(message.author.id) === message.guild.owner.id) return message.channel.send(`${emojis.no}**|** You need to **have** \`MANAGE_GUILD\` **permission** in **${message.guild.name}**.`);
-			message.channel.send(`${emojis.info} **|** You can **control** ${message.guild.name} from this **link**:\nhttps://abayro.xyz/guilds/${message.guild.id}/manage`);
-		} else if (guild) {
-			if (guild.members.get(message.author.id)) {
-				if (!guild.member(message.author).hasPermission('MANAGE_GUILD') || guild.member(message.author.id) === guild.owner.id) return message.channel.send(`${emojis.no}**|** You need to **have** \`MANAGE_GUILD\` **permission** in the **selected server** ( **${message.guild.name}** ).`);
-				message.channel.send(`${emojis.info} **|** You can **control** ${guild.name} from this **link**:\nhttps://abayro.xyz/guilds/${guild.id}/manage`);
-			} else if (!guild.members.get(message.author.id)) {
-				message.channel.send(`${emojis.no} **|** You are not in **${guild.name}**.`);
-			}
-		}
+	exec(message) {
+		return message.channel.send(`${emojis.info} **|** You can **control** ${message.guild.name} from this **link**:\nhttps://abayro.xyz/guilds/${guild.id}/manage`);
 	}
 }
 module.exports = DashboardCommand;
