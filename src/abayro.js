fcb9a10728e4c803f9a8daa24f681fd3c337f6fb
Nouridio <38608933+Nouridio@users.noreply.github.com>
2021-03-27 13:30:01 +0200

    Update abayro.js

--- src/abayro.js
+++ src/abayro.js
@@ -5,7 +5,7 @@ const client = new AbayroClient({ staff });
 
 // if (process.env.NODE_ENV === 'production') {
 // 	const DBL = require('dblapi.js');
-// 	const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ4MTI0NjUxNjA2NTk5MjcwNCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTM3MjY1OTU0fQ.0_yRqi8EI9eS2hr0GFT6xm5tdJKdoyJEFa2kSs9eQMs', client);
+// 	const dbl = new DBL('', client);
 
 // 	dbl.on('posted', () => {
 // 		client.logger.info(`GC Posted`);
